22784749685798fcdbe94e9cc2a3174a68a74b8e
JonatasRodriguesReis <40697229+JonatasRodriguesReis@users.noreply.github.com>
2020-07-11 09:29:29 -0400

    Route get / da API

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const multer = require("multer");
 const multerConfig = require("./config/multer");
 const Post = require("./models/Post");
 
+routes.get("/",(req,res)=>{
+    res.status(200).send("API Upload Midias");
+})
+
 routes.post("/posts",multer(multerConfig).single('file'), async (req,res)=>{
     const {originalname:name,size,key,location:url} = req.file;
     const post = await Post.create({
